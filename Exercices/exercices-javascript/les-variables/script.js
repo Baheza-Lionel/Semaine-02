0c48261b81810274288ecc6680f8020703bd2c6a
Léo <66998902+Baheza-Lionel@users.noreply.github.com>
2020-06-17 13:16:00 +0200

    update

--- Exercices/exercices-javascript/les-variables/script.js
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -4,10 +4,21 @@ const greeting = "Bonjour je m'appelle";
 let prenom = 'Lionel';
 console.log(greeting + ' ' + prenom);
 const me = "j'ai";
-console.log(me);
-let firstNumber = 2;
-let secondNumber = 0;
-console.log(me + ' ' + firstNumber + secondNumber);
-const age = 'ans';
-console.log(age);
-console.log(me + ' ' + firstNumber + secondNumber + ans);
+let age = '20 ans';
+console.log(greeting + ' ' + prenom + ' ' + me + ' ' + age);
+let siriusStudentArray = [
+  'Geremy',
+  'Eudès',
+  'Ezéchiel',
+  'Fanny',
+  'Samantha',
+  'Lionel',
+];
+console.log(siriusStudentArray);
+console.log(siriusStudentArray[5]);
+const student = {
+  nom: 'Baheza',
+  prenom: 'Lionel',
+  age: '20',
+};
+console.log(student.nom);
