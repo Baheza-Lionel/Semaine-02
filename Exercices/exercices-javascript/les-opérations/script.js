56966bf37e7d813d7fb6a0cb28549b8328736c5d
Léo <66998902+Baheza-Lionel@users.noreply.github.com>
2020-06-19 15:20:09 +0200

    update

--- Exercices/exercices-javascript/les-opérations/script.js
+++ Exercices/exercices-javascript/les-opérations/script.js
@@ -1,26 +1,11 @@
-//Insérez ici les exercices 2.1 à 2.4
-
-// Insérez ici les exercices 2.4 à 2.8 en utilisant les variables déjà créées à votre disposition
-
-let firstNumber = 9;
-let secondNumber = 8;
-let thirdNumber = 4;
-let fourthNumber = 12;
-
-// Exercice incrémentation
-function incrementOnClick() {
-    document.getElementById("increment").addEventListener("click", function decrement() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
-        let incrementCounter = 0;
-
-       console.log("Incrémentez") 
-})};
-// Exercice incrémentation
-function decrementOnClick() {
-    document.getElementById("decrement").addEventListener("click", function increment() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice sur la décrémentation, nous verrons plus tard à quoi ces lignes pré-existantes correspondent
-        let decrementCounter = 100;
-
-       console.log("Décrémentez");
-})};
-
+const firstNumber = 1;
+let secondNumber = 2;
+console.log(firstNumber * secondNumber);
+let thirdNumber = 2;
+console.log(secondNumber * thirdNumber);
+let forthNumber = 4;
+console.log(secondNumber * forthNumber);
+let fifthNumber = 8;
+console.log(secondNumber * fifthNumber);
+function divisionBytwo
+return
