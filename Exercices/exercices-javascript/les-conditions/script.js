a86590bfff2078735ac4a5e71b116979c30431ba
Léo <66998902+Baheza-Lionel@users.noreply.github.com>
2020-06-29 12:59:07 +0200

    update

---
+++ Exercices/exercices-javascript/les-conditions/script.js
@@ -0,0 +1,23 @@
+let leftNumber = 8; //numerique
+let rightNumber = 4;
+console.log(rightNumber);
+console.log(leftNumber);
+
+function superieur() {
+  //si la variable rightNumber est plus petite que la variable leftNumber
+  if (rightNumber < leftNumber) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
+}
+
+superieur();
+function superieurToten() {
+  if (leftNumber > 10) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
+}
+superieurToten();
